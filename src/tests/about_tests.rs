24efc2df402cc9047d8dc91b086e444c6b4f7349
cloudy <37777261+uwuclxdy@users.noreply.github.com>
2025-08-21 02:29:42 +0200

    improve all /about commands

--- src/tests/about_tests.rs
+++ src/tests/about_tests.rs
@@ -1,6 +1,7 @@
-use crate::commands::about::{format_duration, BOT_START_TIME};
+use crate::commands::about::BOT_START_TIME;
 use poise::serenity_prelude as serenity;
 use sysinfo::System;
+use crate::utils::format_duration;
 
 #[tokio::test]
 async fn test_format_duration() {
@@ -25,14 +26,14 @@ async fn test_bot_start_time_initialization() {
 #[tokio::test]
 async fn test_database_stats_format() {
     // Test that database stats string formatting works correctly
-    let db_stats = format!("Configs: {}\nRoles: {}\nCooldowns: {}\nDB Guilds: {}", 
+    let db_stats = format!("Configs: {}\nRoles: {}\nCooldowns: {}\nDB Guilds: {}",
         5, 15, 2, 3);
-    
+
     assert!(db_stats.contains("Configs: 5"));
     assert!(db_stats.contains("Roles: 15"));
     assert!(db_stats.contains("Cooldowns: 2"));
     assert!(db_stats.contains("DB Guilds: 3"));
-    
+
     // Check that the format matches our expected pattern
     let lines: Vec<&str> = db_stats.lines().collect();
     assert_eq!(lines.len(), 4);
@@ -43,7 +44,7 @@ fn test_process_id_format() {
     // Test that process ID can be retrieved and formatted
     let pid = std::process::id();
     let process_info = format!("PID: {}", pid);
-    
+
     assert!(process_info.starts_with("PID: "));
     assert!(pid > 0); // Process ID should be positive
 }
@@ -53,7 +54,7 @@ fn test_system_info_structures() {
     // Test that sysinfo System can be created and basic methods work
     let mut sys = System::new_all();
     sys.refresh_all();
-    
+
     // These should not panic
     let _total_memory = sys.total_memory();
     let _used_memory = sys.used_memory();
@@ -69,7 +70,7 @@ fn test_discord_timestamp_format() {
     let test_timestamp = 1640995200; // Jan 1, 2022
     let formatted = format!("<t:{}:F>", test_timestamp);
     assert_eq!(formatted, "<t:1640995200:F>");
-    
+
     let relative_formatted = format!("<t:{}:R>", test_timestamp);
     assert_eq!(relative_formatted, "<t:1640995200:R>");
 }
@@ -81,7 +82,7 @@ fn test_premium_tier_conversion() {
     let tier1 = serenity::PremiumTier::Tier1;
     let tier2 = serenity::PremiumTier::Tier2;
     let tier3 = serenity::PremiumTier::Tier3;
-    
+
     let level0 = match tier0 {
         serenity::PremiumTier::Tier0 => 0,
         serenity::PremiumTier::Tier1 => 1,
@@ -89,7 +90,7 @@ fn test_premium_tier_conversion() {
         serenity::PremiumTier::Tier3 => 3,
         _ => 0,
     };
-    
+
     let level1 = match tier1 {
         serenity::PremiumTier::Tier0 => 0,
         serenity::PremiumTier::Tier1 => 1,
@@ -97,7 +98,7 @@ fn test_premium_tier_conversion() {
         serenity::PremiumTier::Tier3 => 3,
         _ => 0,
     };
-    
+
     let level2 = match tier2 {
         serenity::PremiumTier::Tier0 => 0,
         serenity::PremiumTier::Tier1 => 1,
@@ -105,7 +106,7 @@ fn test_premium_tier_conversion() {
         serenity::PremiumTier::Tier3 => 3,
         _ => 0,
     };
-    
+
     let level3 = match tier3 {
         serenity::PremiumTier::Tier0 => 0,
         serenity::PremiumTier::Tier1 => 1,
@@ -113,7 +114,7 @@ fn test_premium_tier_conversion() {
         serenity::PremiumTier::Tier3 => 3,
         _ => 0,
     };
-    
+
     assert_eq!(level0, 0);
     assert_eq!(level1, 1);
     assert_eq!(level2, 2);
@@ -126,7 +127,7 @@ fn test_memory_calculations() {
     let bytes_1gb = 1024 * 1024 * 1024_u64;
     let mb_from_gb = bytes_1gb / 1024 / 1024;
     assert_eq!(mb_from_gb, 1024);
-    
+
     let total_memory = 8192_u64; // 8GB in MB
     let used_memory = 4096_u64;  // 4GB in MB
     let usage_percent = (used_memory as f64 / total_memory as f64) * 100.0;
@@ -147,25 +148,25 @@ fn test_package_version() {
 fn test_system_info_collection() {
     let mut sys = System::new_all();
     sys.refresh_all();
-    
+
     // Memory information
     let total_memory = sys.total_memory() / 1024 / 1024;
     let used_memory = sys.used_memory() / 1024 / 1024;
-    
+
     assert!(total_memory > 0);
     assert!(used_memory <= total_memory);
-    
+
     // CPU information
     let cpu_usage = sys.global_cpu_usage();
     let cpu_count = sys.cpus().len();
-    
+
     assert!(cpu_usage >= 0.0);
     assert!(cpu_count > 0);
-    
+
     // OS information
     let os_name = System::name();
     let os_version = System::os_version();
-    
+
     // These might be None on some systems, but shouldn't panic
     if let Some(name) = os_name {
         assert!(!name.is_empty());
